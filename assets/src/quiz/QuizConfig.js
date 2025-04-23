fdf4736b33ce8b118f97c774319deb526519d2da
Kaiky Souza <kaikybelga@gmail.com>
2025-04-23 18:13:29 -0300

    Melhora a estrutura do menu de configurações e adiciona suporte a fullscreen com transições suaves

--- assets/src/quiz/QuizConfig.js
+++ assets/src/quiz/QuizConfig.js
@@ -1,3 +1,6 @@
+
+//CONFIG
+
 function toggleConfigMenu() {
   const configBox = document.getElementById("configBox");
   if (configBox.style.display === "block") {
@@ -14,4 +17,30 @@ document
       this.style.transform === "rotate(360deg)"
         ? "rotate(0deg)"
         : "rotate(360deg)";
+  });
+
+
+
+  //FULLSCREEN 
+
+  document.querySelector(".fullscreen").addEventListener("click", function () {
+    if (document.fullscreenElement) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen(); 
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen(); 
+      }
+    } else {
+      
+      const element = document.documentElement;
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      }
+    }
   });
